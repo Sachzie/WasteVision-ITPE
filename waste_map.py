b2ec12baf81e5aed95e1f9a98798e2cee5aeafca
Sachzie <schz.ilagan@gmail.com>
2025-10-10 09:03:14 +0800

    initial commit

---
+++ waste_map.py
@@ -0,0 +1,15 @@
+# waste_map.py
+WASTE_TYPE_MAP = {
+    "bottle": "recyclable",
+    "plastic bottle": "recyclable",
+    "can": "recyclable",
+    "paper": "recyclable",
+    "cardboard": "recyclable",
+    "banana": "biodegradable",
+    "apple": "biodegradable",
+    "orange": "biodegradable",
+    "food": "biodegradable",
+    "battery": "hazardous",
+    "light bulb": "hazardous",
+    "spray can": "hazardous"
+}
