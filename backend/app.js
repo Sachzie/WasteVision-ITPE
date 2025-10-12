00f705d04dbaedb5749e602e906a990b43b80d3e
Sachzie <schz.ilagan@gmail.com>
2025-10-12 19:40:15 +0800

    minimal update like gitignore

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
-
+const path =  require("path")
 app.use(cors())
 app.use(express.json({limit:"500mb"}))
+// app.use(express.static(path.join(__dirname, "../ml_service")));
+
+
 
 module.exports = app
